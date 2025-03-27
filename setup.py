0c86d68d4a7f74afa5d5a5d1667c222dfe473e44
NeoDev <neoforevershog@gmail.com>
2025-03-27 11:57:52 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="infer_rvc_python",
-    version="1.1.0",
+    version="1.2.0",
     description="Python wrapper for fast inference with rvc",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -25,7 +25,7 @@ setup(
         "faiss-cpu==1.7.3",
         "torchcrepe==0.0.20",
         "ffmpeg-python>=0.2.0",
-        "fairseq==0.12.2",
+        "fairseq @ git+https://github.com/One-sixth/fairseq.git",
         "typeguard==4.2.0",
         "soundfile",
         "librosa",
